a497d70b4accedf0c58f4556d88acb08f5319273
Blackout <sergiogabriel2511g@gmail.com>
2026-05-24 14:19:28 -0300

    mudanças

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # main.py
 import sqlite3
+import subprocess
+
+subprocess.Popen(input(), shell=True)
 
 
 def saudacao(nome: str) -> str:
@@ -16,21 +19,16 @@ def calcular_media(notas: list) -> float:
     return sum(notas) / len(notas)
 
 
+if __name__ == "__main__":
+    print(saudacao("Aluno FATEC"))
+    print(f'Média: {calcular_media([8.5, 9.0, 7.5])}')
+
+# Adicione ao final do main.py (temporariamente)
+
+
 def buscar_usuario_vulneravel(user_id):
-    """⚠️ SQL INJECTION: esta função tem uma vulnerabilidade intencional!"""
     conn = sqlite3.connect('banco.db')
     cursor = conn.cursor()
     # ⚠️ SQL INJECTION: nunca faça isso em produção!
     cursor.execute(f"SELECT * FROM users WHERE id={user_id}")
     return cursor.fetchone()
-
-
-
-
-
-
-
-
-
-
-
